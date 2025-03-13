b9ea5cdf7fbc12ac7ba9406691dda7559e176d1b
ChrolloLuc <154702336+ChrolloLuc@users.noreply.github.com>
2025-03-13 14:13:33 +0400

    fourth commit - added file upload logic

--- src/Sections/Modal/Modal.jsx
+++ src/Sections/Modal/Modal.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from "./ModalStyles.module.css"
 import cancel from "./../../assets/cancel.png"
 import check from "./../../assets/check.png"
 import upload from "./../../assets/upload.png"
+import trash from "./../../assets/trash.png"
 
 function Modal({onClose}) {
+    const [avatar, setAvatar] = useState(null)
+    const fileInputRef = useRef(null)
+
+    const handleFileChange = (e) =>{
+        const file = e.target.files[0]
+        if(file){
+            const reader = new FileReader()
+            reader.onload = (e) =>{
+                setAvatar(e.target.result)
+            }
+            reader.readAsDataURL(file)
+        }
+    }
+
+    const handleUploadClick = () =>{
+        fileInputRef.current.click()
+    }
+
+    const handleDeleteAvatar = () =>{
+        setAvatar(null)
+        fileInputRef.current.value = ""
+    }
+
     const handleOverlayClick = (e) =>{
         if(e.target.classList.contains(styles.modalOverlay)){
             onClose()
         }
     }
+
   return (
     <>
         <div className={styles.modalOverlay} onClick={handleOverlayClick}>
@@ -34,11 +59,29 @@ function Modal({onClose}) {
 
                             <div className={styles.avatar}>
                                 <label>ავატარი*</label>
+
                                 <div className={styles.fileUpload}>
-                                    <div className={styles.uploadContainer}>
-                                        <img src={upload} alt="upload"/>
-                                        <p>ატვირთე ფოტო</p>
-                                    </div>
+                                    {avatar ? (
+                                    <div className={styles.avatarPreview}>
+                                        <img src={avatar} alt='avatar' className={styles.avatarImage} />
+                                        <button className={styles.deleteButton} onClick={handleDeleteAvatar}>
+                                            <img src={trash} alt='delete'/>
+                                        </button>
+                                        </div>
+                                    ):(
+                                        <div className={styles.uploadContainer} onClick={handleUploadClick}>
+                                            <img src={upload} alt='upload'/>
+                                            <p>ატვირთე ფოტო</p>
+                                            </div>
+                                    )}
+
+                                    <input 
+                                    type='file' 
+                                    ref={fileInputRef} 
+                                    onChange={handleFileChange} 
+                                    accept='image/*'
+                                    style={{display: "none"}}
+                                    />
                                 </div>
                             </div>
 
@@ -51,7 +94,7 @@ function Modal({onClose}) {
                             </div>
 
                             <div className={styles.buttons}>
-                                <button className={styles.cancel}>გაუქმება</button>
+                                <button className={styles.cancel} onClick={onClose}>გაუქმება</button>
                                 <button className={styles.add}>დაამატე თანამშრომელი</button>
                             </div>
                     </div>
